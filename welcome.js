cc604c8b032216fe076ceab6cb7fb39a7061a8a8
fofoiraq <fofoiraq23@gmail.com>
2018-09-12 22:24:36 -0600

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -100,7 +100,7 @@ client.on("guildMemberAdd", (member) => {
                 if (dat[Inv] < Invite.uses) {
                     console.log(3);
                     console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
- channel.send(` ♥ **تم دعوته من قبل ${Invite.inviter} ♥ `)            
+ channel.send(`تم دعوتك من قبل ${Invite.inviter}`)            
  }
             dat[Inv] = Invite.uses;
         })
